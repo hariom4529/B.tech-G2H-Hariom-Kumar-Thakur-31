bf7332366d3795e6bf4c1b83f72803d961865dd5
Hariom Thakur <fhariom4@gmail.com>
2026-02-20 11:47:31 +0530

    Add files via upload

---
+++ Equal or Not.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main ()
+{
+    int a,b;
+    printf ("Input number a:\n");
+    scanf("%d",&a);
+    printf("Input number b:\n");
+    scanf("%d",&b);
+    if (a==b)
+    {
+    printf("Equal Numbers");
+    }
+    else
+    {
+        printf("Not Equal");
+    }
+    return 0;
+}
