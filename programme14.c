fecbbc64dc103e5c405e0e0ceb44169bd1e4df3b
Hariom Thakur <fhariom4@gmail.com>
2026-04-02 20:22:10 +0530

    Add files via upload

---
+++ programme14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{ int a,b,c=0,d=0;
+printf("enter number");
+scanf ("%d",&a);
+for (b=1; b<-a; b++)
+{
+if (b%2==0)
+{
+    c = c + b;
+}
+else
+{
+    d = d + b;
+}
+}
+printf("sum of even numbers is %d\n", c);
+printf("sum of odd number is %d\n",d);
+return 0;
+} 
---
+++ programme16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{   int n, i, a=1;
+printf("enter number to check: /n");
+scanf("%d",&n);
+for (i=2; i<n ; i++)
+{   if (n%i==0)
+    {
+a=2;
+break;
+    }
+}
+if (a == 1)
+{   printf("number is prime number");
+}
+else
+{
+printf("number is not a prime number");
+}
+return 0;
+}
